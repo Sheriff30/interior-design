53d744208f449397a4a0b06d265009f09a06d9d0
Tarek <tare2fo2ad@gmail.com>
2024-09-25 18:07:07 +0300

    updates

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const beforeBtn = document.querySelector(".before-btn");
 const afterBtn = document.querySelector(".after-btn");
-const beforeAfterImg = document.querySelector(".before-after-img img");
+const beforeImg = document.querySelector(".before-after-img .before-img");
+const AfterImg = document.querySelector(".before-after-img .after-img");
 
 // before after section script
 
 beforeBtn?.addEventListener("click", function () {
-  beforeAfterImg.src = "/assets/before.webp";
   beforeBtn.classList.add("active-btn");
   afterBtn.classList.remove("active-btn");
+  beforeImg.classList.remove("hidden");
+  AfterImg.classList.add("hidden");
 });
 
 afterBtn?.addEventListener("click", function () {
-  beforeAfterImg.src = "/assets/after.webp";
   beforeBtn.classList.remove("active-btn");
   afterBtn.classList.add("active-btn");
+  beforeImg.classList.add("hidden");
+  AfterImg.classList.remove("hidden");
 });
 
 // Menubar
